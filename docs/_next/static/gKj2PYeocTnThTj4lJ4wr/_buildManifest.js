91e213cbb1a1d9ff2b013fd9dd2a4e1098717a19
Reuben Gardos Reid <ReubenJ@users.noreply.github.com>
2021-05-14 14:41:46 -0500

    remove built directory and ignore

--- docs/_next/static/gKj2PYeocTnThTj4lJ4wr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2076583a11707cd3cfba.js"],"/_error":["static/chunks/pages/_error-6f6b72bde07bfb162d97.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
