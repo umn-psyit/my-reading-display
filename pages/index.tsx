f55b3fd2522fcf203d225461e63623334ae847ab
Reuben Gardos Reid <ReubenJ@users.noreply.github.com>
2021-05-18 16:49:59 -0500

    add i18n

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,33 @@
 import Head from 'next/head'
+import type { GetStaticProps } from 'next'
+
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 export default function Home() {
+  const { t } = useTranslation('common');
+
   return (
     <div className="container">
       <Head>
-        <title>Vision Calculator</title>
+        <title>{t('appTitle')}</title>
       </Head>
 
       <main>
+      <h1>{t('appTitle')}</h1>
       </main>
 
       <footer>
       </footer>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  if (context.locale === undefined) {
+    throw new Error('context.locale is undefined');
+  }
+  return {props: {
+    ...(await serverSideTranslations(context.locale, ['common']))
+  }}
 }
