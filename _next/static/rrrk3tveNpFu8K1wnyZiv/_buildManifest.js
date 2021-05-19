34c9b9932b0b014b571a516329094e2ebc047c58
ReubenJ <5456207+ReubenJ@users.noreply.github.com>
2021-05-19 17:40:40 +0000

    Deploying to gh-pages from @ umn-psyit/online-vision-calculator@3d8b93f8bc2c376c13e33fdd439a7266b77a1528 🚀

---
+++ _next/static/rrrk3tveNpFu8K1wnyZiv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/69-44f4b8480eacc5c0dcf7.js","static/chunks/pages/index-8fadd6acfb257aac8250.js"],"/_error":["static/chunks/pages/_error-c5ce589d3f05c3b8e9ef.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
