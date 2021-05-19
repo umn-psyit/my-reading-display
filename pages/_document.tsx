ed2e63db715444043b40c2e1ef3fbd3d9670b632
Reuben Gardos Reid <ReubenJ@users.noreply.github.com>
2021-05-19 13:52:56 -0500

    add lang tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {	
 	render() {
 		return (
-			<Html>
+			<Html lang="en">
         <Head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         </Head>
