4c08737aed01b9d861d55dbdf45876e7092912f3
Reuben Gardos Reid <ReubenJ@users.noreply.github.com>
2021-11-02 14:00:37 +0100

    add pdf package

---
+++ calculator/report.tsx
@@ -0,0 +1,11 @@
+import { PDFDownloadLink, Document, Page } from '@react-pdf/renderer';
+
+const ReportPDF = () => (
+    <Document>
+      <Page>
+        
+      </Page>
+    </Document>
+  );
+
+export default ReportPDF;
