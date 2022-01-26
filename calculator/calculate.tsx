1290e97c818cab72f48e479eb77050f5c7e283a0
Yingzi <51935634+YZ-X@users.noreply.github.com>
2022-01-25 20:19:50 -0600

    Try correcting minpoint 3

--- calculator/calculate.tsx
+++ calculator/calculate.tsx
@@ -260,6 +260,10 @@ export const calculate = (values: InputValuesInterface) => {
   let minPoint: number = -1;
   if ((xf instanceof MinMax) && (wx instanceof MinMax)) {
     minPoint = (0.08 * vd * Math.pow(10, CPS) / xf.max) / Math.pow(2.14, wx.max);
+  } else if ((xf instanceof MinMax) && !(wx instanceof MinMax)) {
+    minPoint = (0.08 * vd * Math.pow(10, CPS) / xf.max) / Math.pow(2.14, wx);
+  } else if (!(xf instanceof MinMax) && (wx instanceof MinMax)) {
+    minPoint = (0.08 * vd * Math.pow(10, CPS) / xf) / Math.pow(2.14, wx.max);
   } else {
     minPoint = (0.08 * vd * Math.pow(10, CPS) / xf) / Math.pow(2.14, wx);
     if (isNaN(minPoint)) {
